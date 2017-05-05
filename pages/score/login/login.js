492f75992d0a59bcd7940ca3b59afe60619bf44c
往日。 <mwr123607@126.com>
2017-05-05 15:38:46 +0800

    完成了登陆页面

--- pages/score/login/login.js
+++ pages/score/login/login.js
@@ -1,10 +1,18 @@
+var Zan = require('../../../dist/index');
+
 var app = getApp()
-Page({
+Page(Object.assign({}, Zan.Switch,{
   data: {
+    user:"",
+    pwd:"",
     year: ['请选择学年', '2016-2017', '2015-2016', '2014-2015', '2013-2014'],
     yearIndex: 0,
     term: ['请选择学期', '1', '2', '3'],
     termIndex: 0,
+    rememberMe: {
+      checked: false
+    },
+    showDialog: false,
   },
   onLoad: function () {
     
@@ -19,4 +27,21 @@ Page({
       termIndex: e.detail.value
     });
   },
-})
+
+  toggleDialog() {
+    this.setData({
+      showDialog: !this.data.showDialog
+    });
+  },
+  
+  handleZanSwitchChange(e) {
+    var componentId = e.componentId;
+    var checked = e.checked;
+
+      // 同步开关
+      this.setData({
+        [`${componentId}.checked`]: checked
+      });
+
+  }
+}))
